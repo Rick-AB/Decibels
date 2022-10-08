6b9b3ad82700308c82c97a62c24bca57011d2620
Richard Bajomo <richardbajomo@gmail.com>
2022-10-08 21:50:08 +0100

    refactored tracks viewModel to get rid of try-catch block

--- app/src/main/java/com/rickinc/decibels/presentation/tracklist/TrackListViewModel.kt
+++ app/src/main/java/com/rickinc/decibels/presentation/tracklist/TrackListViewModel.kt
@@ -1,6 +1,5 @@
 package com.rickinc.decibels.presentation.tracklist
 
-import com.rickinc.decibels.domain.exception.ReadAudioFilesException
 import com.rickinc.decibels.domain.repository.AudioRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -13,13 +12,13 @@ class TrackListViewModel(
     val uiState = _uiState.asStateFlow()
 
     fun getAudioFiles() {
-        try {
-            val result = audioRepo.getAudioFiles()
-            result.onSuccess { tracks ->
+        val result = audioRepo.getAudioFiles()
+        result.fold(
+            onSuccess = { tracks ->
                 _uiState.update { TrackListState.DataLoaded(tracks) }
-            }
-        } catch (e: ReadAudioFilesException) {
-            _uiState.update { TrackListState.Error }
-        }
+            },
+            onFailure = {
+                _uiState.update { TrackListState.Error }
+            })
     }
 }
--- app/src/test/java/com/rickinc/decibels/data/repository/TestAudioRepository.kt
+++ app/src/test/java/com/rickinc/decibels/data/repository/TestAudioRepository.kt
@@ -1,14 +1,13 @@
 package com.rickinc.decibels.data.repository
 
-import com.rickinc.decibels.domain.exception.ReadAudioFilesException
-import com.rickinc.decibels.domain.repository.AudioRepository
 import com.rickinc.decibels.domain.model.Result
+import com.rickinc.decibels.domain.repository.AudioRepository
 import com.rickinc.decibels.presentation.model.Track
 
 class TestAudioRepository : AudioRepository {
     var shouldThrowException: Boolean = false
     override fun getAudioFiles(): Result<List<Track>> {
-        if (shouldThrowException) throw ReadAudioFilesException()
-        else return Result.Success(Track.createDummyTracks())
+        return if (shouldThrowException) Result.Error("Error reading audio files")
+        else Result.Success(Track.createDummyTracks())
     }
 }
