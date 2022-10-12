33d8f25ddd300d0368f2fb981724b6b510882a7c
Richard Bajomo <richardbajomo@gmail.com>
2022-10-12 12:28:53 +0100

    switched to dark theme

--- app/src/main/java/com/rickinc/decibels/presentation/MainActivity.kt
+++ app/src/main/java/com/rickinc/decibels/presentation/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DecibelsTheme {
+            DecibelsTheme(darkTheme = true) {
                 Box(modifier = Modifier.fillMaxSize()) {
                     MainActivityLayout()
                 }
